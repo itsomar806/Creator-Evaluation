14aa9262fcf3eede3eef039ea671b456a0bacead
itsomar806 <rodriguezomar806@gmail.com>
2025-04-22 13:08:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ input[type="text"] {
 </style>
 """, unsafe_allow_html=True)
 
-# Center the logo and input
+# Center the title and input
 st.markdown("<h2 style='text-align: center; color: #FFCD78;'>HubSpot Creator Audit</h2>", unsafe_allow_html=True)
 
 # Handle session state for rerun persistence
